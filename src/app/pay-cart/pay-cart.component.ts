300d57653792fdd12282c0751e3f81e649814c5c
juan pablo <juanpablo@MacBook-Pro.local>
2020-05-30 12:06:37 +0200

    Iconos de formulario contacto. Reservar:formulario

--- src/app/pay-cart/pay-cart.component.ts
+++ src/app/pay-cart/pay-cart.component.ts
@@ -10,7 +10,9 @@ export class PayCartComponent implements OnInit {
 
   constructor(private router: Router) { }
   productsShoppingCart: any[];
-
+  nombre: string='';
+  mail: string = '';
+  telefono: string='';
 
   ngOnInit(): void {
     let productsShoppingCartJSON = sessionStorage.getItem('listCart');
@@ -25,7 +27,12 @@ export class PayCartComponent implements OnInit {
   }
 
   confirm(){
-    alert('Reserva confirmada correctamente');
+    if(this.nombre=='' || this.telefono=='' || this.mail==''){
+      alert('Debe rellenar los datos de contacto antes de solicitar la reserva');
+      return;
+    }
+
+    alert('Reserva confirmada correctamente. Los vendederos se pondrán en contacto con usted lo antes posible. Muchas gracias');
     sessionStorage.setItem('listCart', '');
     this.router.navigate(['/home']);
   }
